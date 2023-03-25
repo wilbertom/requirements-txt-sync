4283e07b8975eb5cf4e2b7b1fc19bdf75bf96e7f
Kenpachi Zaraki <iloveerika8@gmail.com>
2023-03-24 20:56:14 -0500

    had to to delete i = 2

--- verifying_requirements.py
+++ verifying_requirements.py
@@ -5,11 +5,11 @@ from functions_for_program import (
 )
 
 # what if i make this is a whole for loop where it goes by file, instead of putting everything in one?
-i = 2
+
 for file_name in sys.argv[2:]:
     
     file_1 = open(sys.argv[1])
-    file_2 = open(sys.argv[i])
+    file_2 = open(file_name)
 
     requirements_in_file_1 = requirements_used(file_1)
     requirements_in_file_2 = requirements_used(file_2)
@@ -20,4 +20,4 @@ for file_name in sys.argv[2:]:
 
     for warning in warnings:
         print(warning)
-    i += 1
+ 
