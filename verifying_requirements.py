02a0fd82e92565a6c82f84495399c8c51763d021
Kenpachi Zaraki <iloveerika8@gmail.com>
2023-03-24 20:53:11 -0500

    had to to fix positioning of i = 2 and fix the tests because they were failing

--- verifying_requirements.py
+++ verifying_requirements.py
@@ -1,14 +1,13 @@
 import sys
 from functions_for_program import (
     requirements_used,
-    verify_requirements,
-    requirements_used_in_multiple_files,
+    verify_requirements
 )
 
 # what if i make this is a whole for loop where it goes by file, instead of putting everything in one?
-
+i = 2
 for file_name in sys.argv[2:]:
-    i = 2
+    
     file_1 = open(sys.argv[1])
     file_2 = open(sys.argv[i])
 
