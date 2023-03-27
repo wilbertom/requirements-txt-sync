a39c4098a17b3c80cae7e8c108ac2293512a8ed3
Kenpachi Zaraki <iloveerika8@gmail.com>
2023-03-27 04:38:22 -0500

    added an option if you want to make the changes, also removed newline for every requirement

--- verifying_requirements.py
+++ verifying_requirements.py
@@ -21,10 +21,14 @@ for file_name in sys.argv[2:]:
     for warning in warnings:
         print(warning)
 
-    with open(file_name, "w") as file:
-        updated_requirements = replace_requirements(
-            requirements_in_file_1, requirements_in_file_2
-        )
+    stop_changes = input("Would you like to apply these changes? (y,n)")
 
-        for requirement in updated_requirements:
-            file.write(f"{requirement}\n")
+    if stop_changes == 'y':
+
+        with open(file_name, "w") as file:
+            updated_requirements = replace_requirements(
+                requirements_in_file_1, requirements_in_file_2
+            )
+
+            for requirement in updated_requirements:
+                file.write(f"{requirement}")
